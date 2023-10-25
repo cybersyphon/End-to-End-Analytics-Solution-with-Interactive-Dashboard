d9c3355286ed8f13829307d96d21cc53af48a99c
Piyush Juyal <42019920+cybersyphon@users.noreply.github.com>
2023-10-25 11:02:43 +0100

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,131 +1,139 @@
----- Total trips
-
-select count(distinct tripid) from trips 
-
---checking duplicate
-
-select tripid, count(tripid) cnt from trips group by tripid having count(tripid) > 1
+Analysis of Trip Data
+1. Initial Data Exploration
+To get a basic understanding of the dataset.
 
 
---total drivers
-select count(distinct driverid) from trips
-
---total earning 
-select sum(fare) fare from trips
-
--- driver earnings earnings
-select driverid, sum(fare) Earnings from trips group by driverid
-
---total searches 
-select sum(searches) searches from trips_details
-
--- total serches that got estimate
-select sum(searches) searches from trips_details
-
--- 
-
---total driver cancelled
-select count(*)-sum(driver_not_cancelled)  from trips_details
-
---total otp entered
-select  sum(otp_entered)  from trips_details
-
---total end ride
-
-select  sum(end_ride)  from trips_details
-
---cancelled bookings by driver
-Select count(driver_not_cancelled) from trips_details where driver_not_cancelled =0 and customer_not_cancelled =1
-
---cancelled bookings by customer
-Select count(driver_not_cancelled) from trips_details where driver_not_cancelled =0 and customer_not_cancelled =1
+  -- Preview the main tables
+SELECT * FROM trips;
+SELECT * FROM trips_details;
+SELECT * FROM loc;
+SELECT * FROM duration;
+SELECT * FROM payment;
 
 
---average distance per trip
 
-select avg(distance) from trips
-
--- average fare per distance 
-
-select avg(fare) from trips
+2. Basic Metrics
+Determine the fundamental numbers related to trips.
 
 
--- which is the most used payment method 
-select p.method , a.cnt from payment p inner join
-(select faremethod, count(faremethod) cnt from trips group by faremethod ) 
-a on p.id= a.faremethod order by a.cnt desc
--- the highest payment was made through which instrument
-select p.method , a.* from payment p inner join
-(select top 1 * from trips order by fare desc
-)a on p.id=a.faremethod
--- which two locations had the most trips
+-- Total number of trips
+SELECT COUNT(DISTINCT tripid) FROM trips;
 
-select *, dense_rank() over(order by trip desc) rnk from (
-select loc_from, loc_to, count(distinct tripid) trip from trips group by loc_to, loc_from 
-)a
---top 5 earning drivers
-select top 5 * from
-(select *, dense_rank() over (order by earn desc) rnk from
-(select  driverid, sum(fare) earn from trips group by driverid
-)b)c
+-- Check for duplicate trip IDs
+SELECT tripid, COUNT(tripid) AS cnt FROM trips GROUP BY tripid HAVING COUNT(tripid) > 1;
 
--- which duration had more trips
-select duration , rank() over (order by cnt desc)rnk from
-(select duration , count( distinct tripid) cnt from trips group by 
-duration)a
+-- Total number of unique drivers
+SELECT COUNT(DISTINCT driverid) FROM trips;
+
+-- Total earnings across all trips
+SELECT SUM(fare) AS total_earnings FROM trips;
+
+-- Earnings per driver
+SELECT driverid, SUM(fare) AS earnings FROM trips GROUP BY driverid;
 
 
--- which driver , customer pair had more orders
-select * from trips
-select * from
-(select driverid , custid , rank() over (order by trip desc) rnk from (
-select driverid , custid, count(tripid) trip from trips group by driverid , custid
-)a)b where rnk = 1
--- search to estimate rate
+3. Analysis of Trip Details
+Deep dive into the details of the trips.
 
-select  sum(searches_got_estimate)*100.0/sum(searches)  from trips_details 
+  
+-- Total searches 
+SELECT SUM(searches) AS total_searches FROM trips_details;
 
+-- Total searches that received an estimate
+SELECT SUM(searches_got_estimate) AS total_estimated_searches FROM trips_details;
 
--- estimate to search for quote rates
+-- Total driver cancellations
+SELECT COUNT(*) - SUM(driver_not_cancelled) AS driver_cancellations FROM trips_details;
 
+-- Total OTPs entered
+SELECT SUM(otp_entered) AS otp_counts FROM trips_details;
 
-select  sum(searches_for_quotes)*100.0/sum(searches_got_estimate)  from trips_details 
--- quote acceptance rate
+-- Total trips that ended successfully
+SELECT SUM(end_ride) AS successful_trips FROM trips_details;
 
+-- Bookings cancelled by the driver
+SELECT COUNT(driver_not_cancelled) AS driver_cancellations 
+FROM trips_details 
+WHERE driver_not_cancelled = 0 AND customer_not_cancelled = 1;
 
--- quote to booking rate
+-- Bookings cancelled by the customer
 
+SELECT COUNT(customer_not_cancelled) AS customer_cancellations 
+FROM trips_details 
+WHERE driver_not_cancelled = 1 AND customer_not_cancelled = 0;
 
--- booking cancellation rate
+-- Average distance traveled per trip
+SELECT AVG(distance) AS avg_distance FROM trips;
 
-
--- conversion rate
-
-
--- which area got highest trips in which duration
-select   duration , loc_from, cnt ,dense_rank() over(order by cnt desc) rnk    from 
-(select duration,loc_from, count(distinct tripid) cnt from 
-(
-select a.duration , b.loc_from,  b.tripid from trips a inner join trips_details b 
-on a.tripid=b.tripid
-)a group by duration ,loc_from)b 
-
-
--- which area got the highest fares, cancellations,trips,
-select * from trips
-
-select *, DENSE_RANK() over(order by fare desc) rnk from (Select loc_from , sum (fare) fare from trips
-group by loc_from)a
-
-select *, DENSE_RANK() over(order by cnc desc) rnk from (Select loc_from , count(*)- sum(driver_not_cancelled)cnc from trips_details
-group by loc_from)a
+-- Average fare amount
+SELECT AVG(fare) AS avg_fare FROM trips;
 
 
-select *, DENSE_RANK() over(order by trips desc) rnk from (Select loc_from , count(*)trips from trips_details
-group by loc_from)a
+
+4. Advanced Metrics
+Deeper insights into payment methods, trip locations, durations, and conversion rates.
 
 
--- which duration got the highest trips and fares
-select *, DENSE_RANK() over(order by trips desc) rnk from 
-(Select duration , sum(fare) fare, count(distinct tripid) trips from trips
-group by duration)a
+-- Most used payment method
+SELECT p.method, a.cnt 
+FROM payment p 
+INNER JOIN (SELECT faremethod, COUNT(faremethod) AS cnt FROM trips GROUP BY faremethod) a 
+ON p.id = a.faremethod 
+ORDER BY a.cnt DESC;
+
+-- Highest payment value and its associated payment method
+SELECT p.method, a.* 
+FROM payment p 
+INNER JOIN (SELECT TOP 1 * FROM trips ORDER BY fare DESC) a 
+ON p.id = a.faremethod;
+
+-- Locations with the most trips
+ 
+SELECT *, DENSE_RANK() OVER(ORDER BY trip DESC) AS rank 
+FROM (SELECT loc_from, loc_to, COUNT(DISTINCT tripid) AS trip FROM trips GROUP BY loc_to, loc_from) a;
+
+-- Top 5 earning drivers
+SELECT TOP 5 *, DENSE_RANK() OVER(ORDER BY earn DESC) AS rank 
+FROM (SELECT driverid, SUM(fare) AS earn FROM trips GROUP BY driverid) b;
+
+-- Duration with the highest number of trips
+SELECT duration, RANK() OVER(ORDER BY cnt DESC) AS rank 
+FROM (SELECT duration, COUNT(DISTINCT tripid) AS cnt FROM trips GROUP BY duration) a;
+
+-- Driver and customer pairs with the most trips
+ 
+SELECT * 
+FROM (SELECT driverid, custid, RANK() OVER(ORDER BY trip DESC) AS rank 
+      FROM (SELECT driverid, custid, COUNT(tripid) AS trip FROM trips GROUP BY driverid, custid) a) b 
+WHERE rank = 1;
+
+-- Conversion rates
+SELECT SUM(searches_got_estimate) * 100.0 / SUM(searches) AS search_to_estimate_rate FROM trips_details;
+SELECT SUM(searches_for_quotes) * 100.0 / SUM(searches_got_estimate) AS estimate_to_quote_rate FROM trips_details;
+SELECT SUM(searches_got_quotes) * 100.0 / SUM(searches_for_quotes) AS quote_acceptance_rate FROM trips_details;
+
+-- Booking cancellation rate (using CTE)
+WITH CancellationCounts AS (
+    SELECT SUM(1 - driver_not_cancelled) AS driver_cancelled_count, COUNT(*) AS total_bookings
+    FROM trips_details
+)
+SELECT driver_cancelled_count * 1.0 / total_bookings AS booking_cancellation_rate FROM CancellationCounts;
+
+-- Which area had the highest trips in a particular duration
+SELECT duration, loc_from, cnt, DENSE_RANK() OVER(ORDER BY cnt DESC) AS rank 
+FROM (SELECT duration, loc_from, COUNT(DISTINCT tripid) AS cnt FROM trips GROUP BY duration, loc_from) b;
+
+-- Areas with highest fares, cancellations, and trips
+ SELECT *, DENSE_RANK() OVER(ORDER BY fare DESC) AS rank 
+FROM (SELECT loc_from, SUM(fare) AS fare FROM trips GROUP BY loc_from) a;
+
+SELECT *, DENSE_RANK() OVER(ORDER BY cnc DESC) AS rank 
+FROM (SELECT loc_from, COUNT(*) - SUM(driver_not_cancelled) AS cnc FROM trips_details GROUP BY loc_from) a;
+
+SELECT *, DENSE_RANK() OVER(ORDER BY trips DESC) AS rank 
+FROM (SELECT loc_from, COUNT(*) AS trips FROM trips_details GROUP BY loc_from) a;
+
+-- Duration with highest trips and fares
+SELECT *, DENSE_RANK() OVER(ORDER BY trips DESC) AS rank 
+FROM (SELECT duration, SUM(fare) AS fare, COUNT(DISTINCT tripid) AS trips FROM trips GROUP BY duration) a;
+
